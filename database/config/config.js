bf86239b8c46cef2e2e180bdd19ef9ec32427a1b
Mariela Vallecillo <mvalletelt@gmail.com>
2020-12-07 12:02:16 -0300

    vuelvo los datos development back

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports={
   "development": {
-    "username":"b8d0771ad80542",
-    "password": "f0079e7f",
-    "database": "heroku_7ed2a6ace5ffcba",
-    "host": "us-cdbr-east-02.cleardb.com",
+    "username": "cocheria",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
