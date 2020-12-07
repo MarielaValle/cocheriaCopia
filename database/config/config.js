be371459c5e61f8ec470428f6eb0702f1185d5d6
Mariela Vallecillo <mvalletelt@gmail.com>
2020-12-07 11:02:14 -0300

    agrego datos base datos production

--- database/config/config.js
+++ database/config/config.js
@@ -14,9 +14,9 @@ module.exports={
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username":" b8d0771ad80542",
+    "password": "f0079e7f",
+    "database": "heroku_7ed2a6ace5ffcba",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
