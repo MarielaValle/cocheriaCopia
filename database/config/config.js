09e145cb2d0e298785aa5f1e0dba1a9065e5c751
Mariela Vallecillo <mvalletelt@gmail.com>
2020-12-11 10:11:57 -0300

    detalles en detalleObito

--- database/config/config.js
+++ database/config/config.js
@@ -1,8 +1,8 @@
 module.exports={
   "development": {
-    "username": "cocheria",
+    "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "cocheria",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
