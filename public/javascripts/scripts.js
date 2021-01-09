1749ea7b6d07299b83e62d2d2d9b5bf0afcb267f
Mariela Vallecillo <mvalletelt@gmail.com>
2021-01-09 18:24:03 -0300

    agrego carrousel y elimino sucursal

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,70 +1,135 @@
-window.addEventListener("load", function(){
-    
-    const delay=3000;
-    
-    setTimeout (miFuncion, delay);
-    
-    function miFuncion(){
-        if(sessionStorage.getItem('visitante')==null) {
-            
-            
-            let nombre=prompt('Hola! ¿cuál es tu nombre?')
-            
-            let bienvenido = document.querySelector('.bienvenido');  
-            
-            if(nombre==null){
+
+window.addEventListener("load", function () {
+
+    const delay = 3000;
+
+    setTimeout(miFuncion, delay);
+
+    function miFuncion() {
+        if (sessionStorage.getItem('visitante') == null) {
+
+
+            let nombre = prompt('Hola! ¿cuál es tu nombre?')
+
+            let bienvenido = document.querySelector('.bienvenido');
+
+            if (nombre == null) {
                 nombre = 'bienvenid@'
             }
-            
-            bienvenido.innerHTML = '¡Hola, '+ nombre + '!';
-            
-            sessionStorage.setItem('visitante',nombre)
-            
-        }else {
-            nombre=sessionStorage.getItem('visitante')
-            document.querySelector('.bienvenido').innerHTML = '¡Hola ' + nombre +'!';
-            
+
+            bienvenido.innerHTML = '¡Hola, ' + nombre + '!';
+
+            sessionStorage.setItem('visitante', nombre)
+
+        } else {
+            nombre = sessionStorage.getItem('visitante')
+            document.querySelector('.bienvenido').innerHTML = '¡Hola ' + nombre + '!';
+
         }
-        
-        //sessionStorage.clear();
-        
+
+        // sessionStorage.clear();
+
         console.log(sessionStorage);
-        
+
     }
-    
-    
+
+
     //------------------------
     let menuLink = document.querySelectorAll("#menuLink ul li a i");
-    
+
     console.log(menuLink)
-    
-    
-    
-    for (let i=0; i < menuLink.length; i ++){
+
+
+
+    for (let i = 0; i < menuLink.length; i++) {
         //menuLink.onmouseover=function(){
-        
-        menuLink[i].addEventListener("mouseover", function(){              
-            
-                this.style.fontSize="2em"
-                this.style.color= "#9dc3e1"
-           
-                
-                //menuLink.onmouseover=function(){        
-            
-        });
-        
-        menuLink[i].addEventListener("mouseout", function(){              
-            
-            this.style.fontSize="1.5em"
-            this.style.color= "#d98e04"
-       
-            
+
+        menuLink[i].addEventListener("mouseover", function () {
+
+            this.style.fontSize = "2em"
+            this.style.color = "#9dc3e1"
+
+
             //menuLink.onmouseover=function(){        
-        
-    });
-        
+
+        });
+
+        menuLink[i].addEventListener("mouseout", function () {
+
+            this.style.fontSize = "1.5em"
+            this.style.color = "#d98e04"
+
+
+            //menuLink.onmouseover=function(){        
+
+        });
+
     }
+
+
+
+
+    let fotoObituario = document.querySelectorAll('#obituarios section');
+
+    for (let i = 0; i < fotoObituario.length; i++) {
+
+        fotoObituario[i].addEventListener("mouseover", function () {
+
+            this.style.width = "120%"
+
+
+
+
+            //menuLink.onmouseover=function(){        
+
+        });
+
+        fotoObituario[i].addEventListener("mouseout", function () {
+
+            this.style.width = "100%"
+
+
+
+            //menuLink.onmouseover=function(){        
+
+        });
+
+    }
+
+
+
+    let Obituarios = document.querySelector('.tituloO');
+
+   
+
+   
+        let contador = 1;
+        do {
+            Obituarios.addEventListener('mouseout', function () {
+
+                setTimeout (function() {
+                let pregunta = confirm('¿Quieres conocer los planes Pre-Paga Los andes?');
+
+                if (pregunta == true) {
+                    location = 'http://localhost:3000/prepaga';
+                } else {
+
+                    alert('¡Ser precavido está en usted, y Andes está para usted!');
+
+                }
+            },7500)
+            })
+            contador++;
+        } while (contador <= 1)
+       
+
+
+var myCarousel = document.querySelector('#carouselExampleSlidesOnly')
+var carousel = new bootstrap.Carousel(myCarousel)
+
+      //$('.carousel').carousel();
     
+   
 })
 
 
